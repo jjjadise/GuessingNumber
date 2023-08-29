eb8efaf2499a0a50f93d0d31cfecf3b1a80e9a3f
jadise <ljadise@gmail.com>
2023-08-29 14:59:08 -0300

    Add files via upload

---
+++ guessingNumber/guessing-Number-main/Jogo_/script.js
@@ -0,0 +1,37 @@
+var numberToFind = 0;
+var attemps = 0;
+
+function refresh()
+{
+    numberToFind= parseInt(Math.random() *100);
+    console.log(numberToFind)
+}
+
+function verifyNumber(){
+     var bet = document.getElementById('bet').value;
+
+     if( bet > 100 || bet < 1)
+     {
+        alert('Aposta inválida')
+        return;
+     }
+   
+
+ if(bet > numberToFind)
+ {
+     attemps++;
+    alert('O número para ser encontrado é MENOR');
+ }
+
+ else if(bet< numberToFind)
+ {
+    attemps++;
+    alert('O número para ser encontrado é MAIOR');
+ }
+ else
+ {
+    alert('Parabéns você acertou!!!! com ' +attemps+ ' erros')
+ }
+
+}
+refresh();
